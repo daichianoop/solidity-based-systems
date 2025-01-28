0d0428976d6bb0ad9652b4b3eb9d241216e8225b
Anoop <140630377+daichianoop@users.noreply.github.com>
2025-01-28 10:06:21 +0530

    Update simple.sol

--- contracts/simple.sol
+++ contracts/simple.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.0;
 contract SimpleStorage {  
     uint256 private storedNumber;  
 
-    // Function to store a number  
+    
     function setNumber(uint256 _number) public {  
         storedNumber = _number;  
     }  
 
-    // Function to retrieve the stored number  
+      
     function getNumber() public view returns (uint256) {  
         return storedNumber;  
     }  
